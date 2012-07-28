9e943f61a29b661899b6f94c10fc739c4d2cf2ff
Jakub Sikorski <jakubsikorski7@gmail.com>
2012-07-28 23:33:27 +0200

    VariableLengthConverter

---
+++ Source/MidiReader.Tests/VariableLengthConverterTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MidiReader.Tests
+{
+    public class VariableLengthConverterTests
+    {
+        [Test]
+        public void single_byte_value_should_be_correctly_converted()
+        {
+            var singleByteValue = new byte[] {0x5};
+
+            const int expected = 5;
+            int actual = VariableLengthConverter.ToInt32(singleByteValue);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void multiple_bytes_value_should_be_correctly_converted()
+        {
+            CheckIfMultipleBytesValueIsCorrectlyConverted(new byte[] {0xff, 0x7f}, 16383);
+            CheckIfMultipleBytesValueIsCorrectlyConverted(new byte[] {0xf3, 0x3f}, 14783);
+            CheckIfMultipleBytesValueIsCorrectlyConverted(new byte[] {0xfc, 0x4d}, 15949);
+        }
+
+        private static void CheckIfMultipleBytesValueIsCorrectlyConverted(byte[] value, int expected)
+        {
+            int actual = VariableLengthConverter.ToInt32(value);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void converting_invalid_value_should_throw_exception()
+        {
+            var invalidFirstByteValue = new byte[] {0x7f, 0x7f};
+            Assert.Throws<ArgumentException>(() => VariableLengthConverter.ToInt32(invalidFirstByteValue));
+
+            var invalidSecondByteValue = new byte[] { 0xff, 0xff };
+            Assert.Throws<ArgumentException>(() => VariableLengthConverter.ToInt32(invalidSecondByteValue));            
+        }
+
+        [Test]
+        public void converting_empty_bytes_table_should_throw_exception()
+        {
+            var emptyValue = new byte[] {};
+            Assert.Throws<ArgumentException>(() => VariableLengthConverter.ToInt32(emptyValue));
+        }
+
+        [Test]
+        public void value_cannot_be_longer_than_four_bytes()
+        {
+            var tooLongValue = new byte[] {0xff, 0xff, 0xff, 0xff, 0x7f};
+            Assert.Throws<ArgumentException>(() => VariableLengthConverter.ToInt32(tooLongValue));
+        }
+    }
+}
---
+++ Source/MidiReader/VariableLengthConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MidiReader
+{
+    public static class VariableLengthConverter
+    {
+        private const int MaxVariableLengthLength = 4;
+
+        public static int ToInt32(byte[] bytes)
+        {
+            CheckInputArgument(bytes);
+
+            int value = 0;
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                byte currentByte = bytes[i];
+
+                bool isItLAstByteInTable = i == bytes.Length - 1;
+                CheckIfByteIsCorrect(currentByte, isItLAstByteInTable);
+                
+                int variableLengthCurrentByteValue = currentByte & 0x7f;
+                value <<= 7;
+                value |= variableLengthCurrentByteValue;
+            }
+
+            return value;
+        }
+
+        private static void CheckInputArgument(byte[] bytes)
+        {
+            CheckIfInputIsNotEmptyArray(bytes);
+            CheckInputArgumentLength(bytes);
+        }
+
+        private static void CheckIfInputIsNotEmptyArray(byte[] bytes)
+        {
+            if (bytes.Length < 1)
+                throw new ArgumentException("Cannot convert empty bytes table to variable-length value.");
+        }
+
+        private static void CheckInputArgumentLength(byte[] bytes)
+        {
+            if (bytes.Length > MaxVariableLengthLength)
+                throw new ArgumentException("Variable-length value cannot be longer than four bytes.");
+        }
+
+        private static void CheckIfByteIsCorrect(byte @byte, bool isItLastByteInTable)
+        {
+            bool shouldItBeLastByteByFlag = (@byte & 0x7f) == @byte;
+            if (shouldItBeLastByteByFlag != isItLastByteInTable)
+                throw new ArgumentException("Incorrect variable-length notation.");
+        }
+    }
+}
